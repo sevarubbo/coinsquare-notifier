cd7b7b143ddc9d3c705159fdd8fe67e4bc233650
Seva <seva@ada.support>
2021-01-29 18:03:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-alert("Hello");
+(() => {
+  const audio = new Audio("./sound.wav")
+  const observer = new MutationObserver(() => audio.play());
+  
+  const ordersBlock = document.querySelectorAll(".tabbedContainer.FlexBox-eeYpfQ.hgRnHk")[1];
+  
+  if (!ordersBlock) {
+    console.error("Order block not found");
+  }
+  
+  observer.observe(
+    ordersBlock,
+    { attributes: false, childList: true, subtree: true }
+  )
+})()
