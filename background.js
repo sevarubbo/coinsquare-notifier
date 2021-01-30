8df2e61bf0f1aa575972ac08dedf18a1687f745f
Seva <seva@ada.support>
2021-01-29 19:28:00 -0500

    Update background.js

--- background.js
+++ background.js
@@ -2,7 +2,11 @@ const audio = new Audio("./sound.wav")
 
 chrome.runtime.onMessage.addListener((message, callback) => {
   if (message == "orderchange"){
-    chrome.notifications.create({message: "Yoooo"});
+    chrome.notifications.create(`my-notification-${Date.now()}`, {
+        type: "basic",
+        title: "My Title",
+        message: "My Message",
+    });
     
     audio.play();
   }
