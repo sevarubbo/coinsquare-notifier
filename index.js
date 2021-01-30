d089ac7df62b9d47364a3c111188f3faebf8b086
Seva <seva@ada.support>
2021-01-29 19:02:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,18 @@
   const audio = new Audio("./sound.wav")
   const observer = new MutationObserver(() => audio.play());
   
-  const ordersBlock = document.querySelectorAll(".tabbedContainer.FlexBox-eeYpfQ.hgRnHk")[1];
   
-  window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-    console.log(document.querySelectorAll(".tabbedContainer.FlexBox-eeYpfQ.hgRnHk")[1]);
+  window.addEventListener('DOMContentLoaded', () => {
+    const ordersBlock = document.querySelectorAll(".tabbedContainer.FlexBox-eeYpfQ.hgRnHk")[1];
+  
+    if (!ordersBlock) {
+      console.error("Order block not found");
+      return;
+    }
+
+    observer.observe(
+      ordersBlock,
+      { attributes: false, childList: true, subtree: true }
+    )
   });
-  
-  if (!ordersBlock) {
-    console.error("Order block not found");
-    console.log(document.querySelectorAll("*"));
-    return;
-  }
-  
-  observer.observe(
-    ordersBlock,
-    { attributes: false, childList: true, subtree: true }
-  )
 })()
