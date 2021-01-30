99c25cfd2524af2c5896903003805665ddcdbe07
Seva <seva@ada.support>
2021-01-29 23:12:16 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,13 +1,24 @@
 const audio = new Audio("./sound1.wav")
 
 chrome.runtime.onMessage.addListener((message, callback) => {
-  if (message == "orderchange"){
-    chrome.notifications.create(`my-notification-${Date.now()}`, {
-        type: "basic",
-        title: "Что-то",
-        iconUrl: "./icon.png",
-        message: "Что-то произошло",
-    });
+  if (message.type == "orderchange"){
+    if (message.message === "neworder") {
+      chrome.notifications.create(`my-notification-${Date.now()}`, {
+          type: "basic",
+          title: "Ну что ж...",
+          iconUrl: "./icon.png",
+          message: "Заказ размещен",
+      });
+    }
+    
+    if (message.message === "orderfulfilled") {
+      chrome.notifications.create(`my-notification-${Date.now()}`, {
+          type: "basic",
+          title: "Мои поздравления",
+          iconUrl: "./icon.png",
+          message: "Сделка совершена",
+      });
+    }
     
     audio.play();
   }
