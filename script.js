59b9de283dac0d1094dd475e63a7c796941eb89b
Seva <seva@ada.support>
2021-02-02 20:26:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ document.getElementById("volume-input").addEventListener("change", (e) => {
     data: {volume: e.target.value},
   });
 });
+
+chrome.storage.sync.get(['volume'], (result) => {
+  document.getElementById("volume-input").value = result.volume !== undefined ? result.volume : 0.5;
+});
